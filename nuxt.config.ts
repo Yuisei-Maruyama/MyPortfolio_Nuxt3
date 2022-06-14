c0f7049e800aaf10f90d4a73b06f4fafa6a6dd14
Yuisei-Maruyama <yuisei.metro@gmail.com>
2022-06-14 22:47:49 +0900

    add: 基本設定の追加

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,12 @@
 import { defineNuxtConfig } from 'nuxt'
+import { resolve } from 'path'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
+  srcDir: 'src/', // src/ディレクトリ以下にpages/やlayouts/を置けるように設定する
+  alias: {
+    '@': resolve(__dirname, '.src'),
+  },
   typescript: {
     strict: true
   }
